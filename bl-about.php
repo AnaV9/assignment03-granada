0ffc8e0cc732aa2bc676a3d1dd294431d6570ee9
Ana <avaquera@u.rochester.edu>
2018-02-23 21:14:11 -0500

    update

--- bl-about.php
+++ bl-about.php
@@ -25,9 +25,14 @@
 
             <!--About and their work experience, two seperate headings-->
             <h3>About</h3>
-                <p></p>
+                <p>Charlie studies in University of Rochester, double majoring in psychology and computer science. During her time in Rochester, she passionately devotes to social and developmental psychology and human computer interaction. Her interest in developmental psychology led her to contribute in the university kid's lab, while her knowledge in computer science helps her come up with solutions to meliorate the behavior coding system in psychology researches. She also has research experience in Zhejiang University , where she assisted similar developmental psychology researches on children's temperament. Charlie is enthusiastic about child development and hopes to turn this passion into a career.</p>
+
+                <p>I am currently double majoring in psychology and computer science, which gives me 2 different sets of problem-solving techniques. My academic background in computational coding inspired me a lot when I do behavior coding and helps me coming up with ways to optimize the coding system. Also, due to the nature of most of the computer science classes, I have a lot of experience working on teams for projects.</p>
+
             <h3>Work Experience</h3>
-                <p></p>
+                <p>As for my research experiences, last summer I was a research assistant at Zhejiang University’s developmental psychology lab. I scheduled meetings with families while doing behavior coding, and constantly came up with questions and solutions which made the experiment more rigorous and precise. For example, there was a task in the experiment where experimenter came into the room and pretend to be injured. The goal of the task was to measure child’s level of empathy. However, the original design was the parents sit on a chair in the room with the child and they were told to do nothing. I immediately raised question about the possibility that children’s action might be affected by parents doing nothing. Afterwards, I proposed to add an indicator in the coding system which measures how many times the child look back and forth between the “injured” experimenter and the parent, which can show the level of behavior inhibition due to parent’s presence. The research team took my proposal and modified their coding system.</p>
+
+                <p>As for my experiences with kids, I have been doing volunteer work since I was 13. I had experience of teaching in summer school when I was 16. Also, in high school I started up a project to help out the local orphanage by organizing volunteers visiting the orphanage once a week and plan activities to raise money for the kids. I also had experiences working with families when I was a research assistant. I enjoy working with kids and have the ability to answer parents’ concerns in a professional setting.</p>
         </article>
 
         <!--Interesting Facts, in bullet points and their intitial role-->    
@@ -39,7 +44,8 @@
                     <li></li>
                 </ul>
             <h3>Initial Role</h3>
-                <p></p>
+                <h4>Coder</h4>
+                <p>I choose to be the coder for the first assignment because it was my least confident role. Actually, I had experience in CSC 210 last semester and were pretty familiar with both frontend and backend web programing. However, the coding part of web design are still my least favorite ones. According to my team work experiences in last fall, teamwork demands lots of effort from each team member to help out other teammates and collaborate so that the website is functional. Having the idea that no single role will be entirely independent, I decided to take the hard work first and help my teammates get on track. Also, I still need a bit time to familiarize with the idea of a 3-person team.</p>
         </aside>
 
         <?php include "inc/footer.inc";?>
--- index.php
+++ index.php
@@ -24,21 +24,21 @@
 				<img src="images/aa.jpg" alt="New York City Skyline">
 				<figcaption>New York City Skyline</figcaption>
 			</figure>
-			<p><a href = "aa-about.php"> Read More</a></p><!---insert small about me-->
+			<p>My name is Alejandro Abreu, and I was born on September 6th, 1995. I was born in Monte Sinai hospital in Manhattan, and a few months later moved to the Bronx where I currently reside. I am from New York City, more specifically, the Bronx. As a youth, I was very active and in love with sports. I spent most of my time at the park or at the gym either playing baseball or basketball. <a href = "aa-about.php"> Read More</a></p><!---insert small about me-->
 
 		<h2>Casey Brentnall</h2>
 			<figure> <!--images of place where each person is from, round-->
 				<img src="images/cb.jpg" alt="Rochester City Skyline">
 				<figcaption>Rochester City Skyline</figcaption>
 			</figure>
-			<p><a href = "cb-about.php"> Read More</a></p><!---insert small about me-->
+			<p>I was born in Farmington, New York. Farmington is a town located in the northern part of Ontario County, New York, United States. The population was 11,825 at the 2010 census. Farmington is about twenty-five miles southeast of Rochester, New York. The town was named after Farmington, Connecticut. Farmington was part of the Phelps and Gorham Purchase. The town was formed along with its county in 1789, and settlement of the region began the same year. <a href = "cb-about.php"> Read More</a></p><!---insert small about me-->
 
 		<h2>Bei Lu</h2>
 			<figure> <!--images of place where each person is from, round-->
 				<img src="images/bl.jpg" alt="Shanghai City Skyline">
 				<figcaption>Shanghai City Skyline</figcaption>
 			</figure>
-			<p><a href = "bl-about.php"> Read More</a></p><!---insert small about me-->
+			<p>Charlie studies in University of Rochester, double majoring in psychology and computer science. During her time in Rochester, she passionately devotes to social and developmental psychology and human computer interaction. Her interest in developmental psychology led her to contribute in the university kid's lab, while her knowledge in computer science helps her come up with solutions to meliorate the behavior coding system in psychology researches. She also has research experience in Zhejiang University , where she assisted similar developmental psychology researches on children's temperament. <a href = "bl-about.php"> Read More</a></p><!---insert small about me-->
 
 		<?php include "inc/footer.inc"; ?>
 		</div> <!--.container -->
